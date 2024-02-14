33e6b4a4fc5e4b11d177d307a3c1ed616ba6dd5d
Eduard Beukes <edpbeukes@gmail.com>
2024-02-14 09:31:41 +0200

    Added some more ideas to the design doc.

--- UnityProject/Assets/DesignIdeas.cs
+++ UnityProject/Assets/DesignIdeas.cs
@@ -1,10 +1,23 @@
 public class DesignIdeas
 {
     /*
-     * 1) There needs to be a quiet phase of building and researching.
-     * 2) The game should play more like an ARPG than a typical shooter.
+     * 01) There needs to be a quiet phase of building and researching.
+     * 02) The game should play more like an ARPG than a typical shooter.
      *      a) Enemies should have some predictable behaviours.
      *      b) Guns should somehow represent abilities more than just a weapon.
-     * 3) Movement should play a major factor in the game, being able to quickly go different places.
+     * 03) Movement should play a major factor in the game, being able to quickly go different places.
+     * 04) Have 2 guns on the player, but have multiple guns they can choose from...essentially setup loadouts.
+     * 05) Weapon themes are steam punk or diesel punk.
+     * 06) Flame thrower weapon
+     * 07) Grenade launcher weapon
+     * 08) Elephant gun(shotgun)
+     * 09) Player character can upgrade stuff like movement speed and jump height.
+     * 10) Player can increase their health.
+     * 11) The player will keep grabbing stuff, killing auto gives some things...but pickups will also be a thing.
+     * 12) Experience??? I don't like the idea...but maybe.
+     * 13) Laser style gun with overheating mechanics.
+     * 14) Powerful six shooter.
+     * 15) Ammo management? 
+     *
      */
 }
