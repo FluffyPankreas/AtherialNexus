5098369dfa58e6f600511df3a982d58161e17d0f
Eduard Beukes <edpbeukes@gmail.com>
2024-02-14 09:16:03 +0200

    Added doc for design ideas.

---
+++ UnityProject/Assets/DesignIdeas.cs
@@ -0,0 +1,10 @@
+public class DesignIdeas
+{
+    /*
+     * 1) There needs to be a quiet phase of building and researching.
+     * 2) The game should play more like an ARPG than a typical shooter.
+     *      a) Enemies should have some predictable behaviours.
+     *      b) Guns should somehow represent abilities more than just a weapon.
+     * 3) Movement should play a major factor in the game, being able to quickly go different places.
+     */
+}
