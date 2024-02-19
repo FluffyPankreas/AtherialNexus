f13b9d3845c13f8bc045e9fb513cda4283fbf9fd
Eduard Beukes <edpbeukes@gmail.com>
2024-02-19 20:31:52 +0200

    Implemented a naive solution for ammo.

--- UnityProject/Assets/Scripts/DarkMushroomGames/Ammo.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/Ammo.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 
@@ -8,5 +9,9 @@ namespace DarkMushroomGames
     /// </summary>
     public class Ammo : MonoBehaviour
     {
+        public void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
     }
 }
