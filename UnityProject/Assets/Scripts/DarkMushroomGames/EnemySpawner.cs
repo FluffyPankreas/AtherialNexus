627d091605d3420bfc4d7c5dd96cde73d6c6d491
Eduard Beukes <edpbeukes@gmail.com>
2024-02-21 19:43:45 +0200

    Added enemy spawners script framework.

---
+++ UnityProject/Assets/Scripts/DarkMushroomGames/EnemySpawner.cs
@@ -0,0 +1,35 @@
+using System;
+using UnityEngine;
+
+namespace DarkMushroomGames
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField,Tooltip("The enemy prefab that will be spawned.")]
+        private Enemy enemyPrefab;
+
+        [SerializeField, Tooltip("The amount of time that has to elapse before the next enemy will spawn.")]
+        private float spawnCooldown;
+
+        private float _currentCooldown;
+
+        public void Start()
+        {
+            _currentCooldown = spawnCooldown;
+        }
+        public void Update()
+        {
+            _currentCooldown -= Time.deltaTime;
+            if (_currentCooldown <= 0)
+            {
+                CreateNewEnemy();
+                _currentCooldown = spawnCooldown;
+            }
+        }
+
+        private void CreateNewEnemy()
+        {
+            
+        }
+    }
+}
