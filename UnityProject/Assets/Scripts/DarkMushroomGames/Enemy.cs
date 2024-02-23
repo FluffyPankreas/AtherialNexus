c7318d1bccfa67b6624b4b294cb4eb586f858d88
Eduard Beukes <edpbeukes@gmail.com>
2024-02-23 10:19:18 +0200

    Removed the "attack" part from the enemies script.

--- UnityProject/Assets/Scripts/DarkMushroomGames/Enemy.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/Enemy.cs
@@ -14,28 +14,13 @@ namespace DarkMushroomGames
         [SerializeField, Tooltip("The target the agent will track.")]
         private Transform target;
 
-        [SerializeField,Tooltip("The attack distance of the agent.")]
-        private float attackDistance;
-
         [SerializeField, Tooltip("Damage done with attacks.")]
         private int attackDamage;
-        
-        [SerializeField, Tooltip("The location the attack happens.")]
-        private Transform attackLocation;
-
-        
 
         [SerializeField] private LayerMask attacksHitLayer;
-        [SerializeField] private float attackRadius;
 
-        [SerializeField]
-        private float telegraphingTime = 1f;
-        [SerializeField]
-        private float attackTime = 0.2f;
-        
         private HitPoints _hitPoints;
         private NavMeshAgent _navMeshAgent;
-        private bool _attacking = false;
 
         
         public void Awake()
@@ -47,50 +32,23 @@ namespace DarkMushroomGames
             
             _hitPoints = GetComponent<HitPoints>();
             _navMeshAgent = GetComponent<NavMeshAgent>();
-
-            _navMeshAgent.stoppingDistance = attackDistance;
         }
 
         public void Update()
         {
             _navMeshAgent.destination = target.position;
 
-            if (_navMeshAgent.remainingDistance <= attackDistance && !_attacking)
-            {
-                _navMeshAgent.isStopped = true;
-                _attacking = true;
-                TelegraphAttack();
-            }
-            
             if (_hitPoints.HitPointsLeft <= 0)
             {
                 Destroy(gameObject);
             }
         }
 
-        private void TelegraphAttack()
-        {
-            Invoke(nameof(Attack),telegraphingTime);
-        }
-        
         private readonly Collider[] _results = new Collider[2];
-        private void Attack()
-        {
-            var hits = Physics.OverlapSphereNonAlloc(attackLocation.position, attackRadius, _results, attacksHitLayer);
-            for (int i = 0; i < hits; i++)
-            {
-                var hp = _results[i].GetComponent<HitPoints>();
-                hp.SubtractHitPoints(attackDamage);
-            }
-
-
-            Invoke(nameof(ActivateAgent), attackTime);
-        }
 
         private void ActivateAgent()
         {
             _navMeshAgent.isStopped = false;
-            _attacking = false;
         }
     }
 }
