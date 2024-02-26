fd1e781cc3470653451eea5f3d25bcccb50d6e1f
Eduard Beukes <edpbeukes@gmail.com>
2024-02-26 12:31:12 +0200

    Added some additional functionality to souind effects manager.

--- UnityProject/Assets/Scripts/DarkMushroomGames/Managers/SoundEffectsManager.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/Managers/SoundEffectsManager.cs
@@ -8,8 +8,14 @@ namespace DarkMushroomGames.Managers
     {
         [SerializeField,Tooltip("The prefab used to play sound effects at a point in the world.")]
         private AudioSource soundEffectPrefab;
-        
-        public void PlaySoundEffectsClip(AudioClip audioClip, Transform spawnTransform, float volume)
+
+        /// <summary>
+        /// Plays an audio clip at the provided position.
+        /// </summary>
+        /// <param name="audioClip">The audio clip to be played.</param>
+        /// <param name="spawnTransform">The location that the audio source will be played at.</param>
+        /// <param name="volume">The volume at which to play the clip. Default is 1.</param>
+        public void PlaySoundEffectsClip(AudioClip audioClip, Transform spawnTransform, float volume = 1f)
         {
             var audioSource = Instantiate(soundEffectPrefab, spawnTransform.position, quaternion.identity);
             audioSource.clip = audioClip;
@@ -18,6 +24,18 @@ namespace DarkMushroomGames.Managers
 
             var clipLength = audioClip.length;
             Destroy(audioSource.gameObject, clipLength);
+        }
+        
+        /// <summary>
+        /// Plays a random clip from the given clips at the provided position.
+        /// </summary>
+        /// <param name="audioClip">The audio clips to select from to be played.</param>
+        /// <param name="spawnTransform">The location that the audio source will be played at.</param>
+        /// <param name="volume">The volume at which to play the clip. Default is 1.</param>
+        public void PlaySoundEffectsClip(AudioClip[] audioClip, Transform spawnTransform, float volume = 1f)
+        {
+            var index = UnityEngine.Random.Range(0, audioClip.Length);
+            PlaySoundEffectsClip(audioClip[index], spawnTransform, volume);
         }
     }
 }
