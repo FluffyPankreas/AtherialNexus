e1853db2068bea02a98f5a1f75ce45165524dfee
Eduard Beukes <edpbeukes@gmail.com>
2024-02-28 15:59:18 +0200

    Updated player out of bounds to work a bit better.

--- UnityProject/Assets/Scripts/DarkMushroomGames/PlayerOutOfBounds.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/PlayerOutOfBounds.cs
@@ -5,11 +5,17 @@ namespace DarkMushroomGames
 {
         public class PlayerOutOfBounds : MonoBehaviour
         {
+            private Vector3 _startingPosition;
+            public void Start()
+            {
+                _startingPosition = GameObject.FindWithTag("Player").transform.position;
+
+            }
                 public void OnCollisionEnter(Collision collision)
                 {
                     if (collision.gameObject.CompareTag("Player"))
                     {
-                        collision.transform.position = Vector3.zero;
+                        collision.transform.position = _startingPosition;
                     }
                 }
         }
