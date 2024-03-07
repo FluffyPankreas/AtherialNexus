80b2fd2437d41eca6f10b59cc39207fea54e8770
Eduard Beukes <edpbeukes@gmail.com>
2024-03-07 05:50:00 +0200

    Updated the design ideas with some more categories and also added some new ideas.

--- UnityProject/Assets/DesignIdeas.cs
+++ UnityProject/Assets/DesignIdeas.cs
@@ -8,41 +8,55 @@ public class DesignIdeas
      * 03) Movement should play a major factor in the game, being able to quickly go different places.
      * 04) Have 2 guns on the player, but have multiple guns they can choose from...essentially setup loadouts.
      * 05) Weapon themes are steam punk or diesel punk.
-     * 06) Flame thrower weapon
-     * 07) Grenade launcher weapon
-     * 08) Elephant gun(shotgun)
-     * 09) Player character can upgrade stuff like movement speed and jump height.
-     * 10) Player can increase their health.
-     * 11) The player will keep grabbing stuff, killing auto gives some things...but pickups will also be a thing.
-     * 12) Experience??? I don't like the idea...but maybe.
-     * 13) Laser style gun with overheating mechanics.
-     * 14) Powerful six shooter.
-     * 15) Ammo management? 
-     * 16) Going with the Limbo idea, the player needs to be able to prep for the next round, so while the exact level won't be known we should show the next "reality" or something that they'll enter. Or the player should be able to choose the reality they want to go to.
-     * 17) Poison gun(good as a weapon to kill bosses),
-     *      a) Fires blobs that turn into poison clouds that deals damage over time(to the player as well)
-     *      b) Fires darts that stack damage as you apply more darts.
-     * 18) Running/Jumping uses stamina.
-     * 19) Block/Parrying as a mechanic.
-     * 20) Are we going to roll stats for monsters or keep them all exactly the same?
-     * 21) Lightning gun
-     *      a) Jumps from enemy to enemy.
-     * 22) "Expanding" level by player actions. Think bridges in D3.
-     * 23) Environmental things that allows the player to do extra damage.
+     * 06) Player character can upgrade stuff like movement speed and jump height.
+     * 07) Player can increase their health.
+     * 08) The player will keep grabbing stuff, killing auto gives some things...but pickups will also be a thing.
+     * 09) Experience??? I don't like the idea...but maybe.
+     * 10) Ammo management? 
+     * 11) Going with the Limbo idea, the player needs to be able to prep for the next round, so while the exact level won't be known we should show the next "reality" or something that they'll enter. Or the player should be able to choose the reality they want to go to.
+     * 12) Running/Jumping uses stamina.
+     * 13) Block/Parrying as a mechanic.
+     * 14) Are we going to roll stats for monsters or keep them all exactly the same?
+     * 15) "Expanding" level by player actions. Think bridges in D3.
+     * 16) Environmental things that allows the player to do extra damage.
      */
-
-
-
+    
 
 }
 
 public class LoreIdeas
-
 { 
 
 /* 
  * 01) You are in a simulated version of the world you playing in. The truth is too horrifying to actually experience. Hence the pleasant colors and soft surfaces. But occasionally when you take damage you could see a glimpse of what it actually looks like, which could be horrifying and cool. You start at the Nexus in Limbo where are realites come together and are simulated per a lifeform basis. But the corruption has started to spread, and the avatars are presentated as robots. The truth is far too terrifying and beyond human comprehension
-
-
 */
+
 }
+
+public class WeaponIdeas
+{
+    /*
+     * 01) Flame thrower weapon
+     * 02) Grenade launcher weapon
+     * 03) Elephant gun(shotgun)
+     * 04) Laser style gun with overheating mechanics.
+     * 05) Powerful six shooter.
+     * 06) Poison gun(good as a weapon to kill bosses),
+     *      a) Fires blobs that turn into poison clouds that deals damage over time(to the player as well)
+     *      b) Fires darts that stack damage as you apply more darts.
+     * 07) Lightning gun
+     *      a) Jumps from enemy to enemy.
+     *      b) secondary fire does a nova around the player that stuns things.
+     *      c) Secondary fire charges up by using the primary fire.
+     * 08) A weapon that creates turrets of some description. 
+     */
+}
+
+public class EnemyIdeas
+{
+    /*
+     * 1) Have some enemies be "sturdy", ie. require a certain minimum amount of damage before they take any.
+     * 2) Have some enemies lay tripwire traps, or just have tripwire style traps in the game?
+     */
+}
+
