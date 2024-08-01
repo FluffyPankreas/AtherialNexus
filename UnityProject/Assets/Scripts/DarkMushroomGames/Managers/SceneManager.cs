a63e2dbf5acce7c7baf24f9f10a32ddbb843e6ee
Eduard Beukes <edpbeukes@gmail.com>
2024-08-01 16:27:41 +0200

    Initial Scene loading
    Setup the initial scene loading.

--- UnityProject/Assets/Scripts/DarkMushroomGames/Managers/SceneManager.cs
+++ UnityProject/Assets/Scripts/DarkMushroomGames/Managers/SceneManager.cs
@@ -9,10 +9,16 @@ namespace DarkMushroomGames.Managers
         [SerializeField,Tooltip("The scene that has to be loaded that has all high level managers.")]
         private string managersSceneKey;
 
+        [SerializeField,Tooltip("The scene that is the initial phase of the game, spawning the player in the hub.")]
+        private string hubSceneKey; 
+
         public void Start()
         {
             Debug.Log("Loading the Managers Scene.");
             UnityEngine.SceneManagement.SceneManager.LoadScene(managersSceneKey, LoadSceneMode.Additive);
+
+            Debug.Log("Loading the Hub.");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(hubSceneKey, LoadSceneMode.Additive);
         }
 
         public static void LoadScene(int sceneBuildNumber)
